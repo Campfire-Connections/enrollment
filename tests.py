6e96c360180afef2e5591dc4e2811f2c98d31ae0
David Durost <david.durost@gmail.com>
2025-11-19 19:43:32 -0500

    Verify course string/requirement relationships in tests

--- tests.py
+++ tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from core.tests import BaseDomainTestCase
 
-# Create your tests here.
+
+class OrganizationEnrollmentTests(BaseDomainTestCase):
+    def test_get_courses_returns_related_instances(self):
+        courses = self.org_enrollment.get_courses()
+        self.assertIn(self.org_course, courses)
